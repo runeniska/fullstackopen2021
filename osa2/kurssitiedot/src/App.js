e823b055921383027539cbd8cafe18d36b3b764a
Rune Niska <niska.rune@gmail.com>
2021-08-19 10:00:14 +0300

    Create module for Course with related subcomponents

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,49 +1,5 @@
 import React from 'react'
-
-const Header = ({ name }) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map( part => 
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce( (sum, part) => sum + part.exercises, 0)
-  return (
-    <div>
-      Total of {total} exercises
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
