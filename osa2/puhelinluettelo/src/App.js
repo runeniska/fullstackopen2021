7bb6f403c298c6d9f3e139b2b3640c6e3791f511
Rune Niska <niska.rune@gmail.com>
2021-08-20 11:15:21 +0300

    Support adding numbers to persons and prevent duplicates

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,23 +2,37 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.find( person => person.name === newName )) {
-      window.alert(`${newName} is already added to phonebook`)
+    const nameExists = persons.map( person => person.name ).includes(newName)
+    const numberExists = persons.map( person => person.number ).includes(newNumber)
+
+    if (nameExists || numberExists) {
+      window.alert(
+        (nameExists ? newName : newNumber) +
+        ' is already added to phonebook'
+      )
     } else {
-      setPersons(persons.concat({ name: newName }))
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
@@ -28,12 +42,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map( person => <li key={person.name}>{person.name}</li> )}
+        {persons.map( person =>
+          <li key={person.name}>{person.name} {person.number}</li> )}
       </ul>
     </div>
   )
