2ed3640236d0238e8597e120b72d7586d0b400e0
Rune Niska <niska.rune@gmail.com>
2021-08-20 10:30:24 +0300

    Prevent adding same name twice

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,8 +8,12 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
-    setNewName('')
+    if (persons.find( person => person.name === newName )) {
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+    }
   }
 
   const handleNameChange = (event) => {
